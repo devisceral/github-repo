c5fa966ab20825fddd278d19407b80af577a32d7
devisceral <develmendes@gmail.com>
2023-02-27 15:25:57 -0300

    axios and search repositories

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,54 @@
-import React from 'react'
-import {Container, Form, SubmitButton} from "./styles"
-import {FaGithub, FaPlus} from "react-icons/fa";
+import React, { useState, useCallback } from "react";
+import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus } from "react-icons/fa";
+import api from "../../services/api";
 
 const Main = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    async function submit() {
+      const response = await api.get(`repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      setRepositorios([...repositorios, data]);
+      setNewRepo("");
+    }
+
+    submit();
+  }, [newRepo, repositorios]);
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
     <Container>
       <h1>
-       <FaGithub size={25} />
+        <FaGithub size={25} />
         Meus Repositórios
       </h1>
 
-      <Form onSubmit={()=>{}}>
-        <input type="text" placeholder="Adicionar repositórios" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositórios"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
         <SubmitButton>
-          <FaPlus color="#FFF" size={14}  />
+          <FaPlus color="#FFF" size={14} />
         </SubmitButton>
-
       </Form>
     </Container>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
